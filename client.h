21768ffec1dd2e213d519f620be6e668c2167dc1
Archangel <781446156@qq.com>
2024-11-04 20:26:39 +0800

    client header file added

---
+++ client.h
@@ -0,0 +1,39 @@
+/*** 
+ * @ ┌─────────────────────────────────────────────────────────────┐
+ * @ │┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐│
+ * @ ││Esc│!1 │@2 │#3 │$4 │%5 │^6 │&7 │*8 │(9 │)0 │_- │+= │|\ │`~ ││
+ * @ │├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤│
+ * @ ││ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{[ │}] │ BS  ││
+ * @ │├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤│
+ * @ ││ Ctrl │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  ││
+ * @ │├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤│
+ * @ ││ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│Shift │Fn ││
+ * @ │└─────┬──┴┬──┴──┬┴───┴───┴───┴───┴───┴──┬┴───┴┬──┴┬─────┴───┘│
+ * @ │      │Fn │ Alt │         Space         │ Alt │Win│   HHKB   │
+ * @ │      └───┴─────┴───────────────────────┴─────┴───┘          │
+ * @ └─────────────────────────────────────────────────────────────┘
+ * @
+ * @Author: Archangel 781446156@qq.com
+ * @Date: 2024-11-04 20:13:17
+ * @LastEditTime: 2024-11-04 20:13:18
+ * @LastEditors: Archangel 781446156@qq.com
+ * @Description: 
+ * @FilePath: \LANChatRoom\client.h
+ * @Copyright (c) 2024 by Archangel email: 781446156@qq.com, All Rights Reserved. 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define BUFFSIZE 128
+#define HOST_IP "192.168.159.3"
+#define PORT 8888
+
+int sockfd;
+void snd();
+void get_help();
